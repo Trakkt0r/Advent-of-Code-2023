66e38bf3bdb545234923e5c0e131b0770441ad1a
trakktor <107813840+Trakkt0r@users.noreply.github.com>
2023-12-12 04:06:31 +0100

    Uploading solution to Day 11

--- Day 11/main.py
+++ Day 11/main.py
@@ -1,6 +1,6 @@
 from itertools import product, combinations
 
-with open("Day 11\input_test.txt", "r") as input_file:
+with open("Day 11\input.txt", "r") as input_file:
     main_input = [line.replace("\n", "") for line in input_file]
 
     # everything below in this scope is responsible for adding in the expansions of the universe
@@ -16,15 +16,13 @@ with open("Day 11\input_test.txt", "r") as input_file:
 
     for column in sorted(list(empty_columns), reverse=True):
         for line in main_input:
-            main_input[main_input.index(line)] = line[:column] + "X" + line[column:]
+            main_input[main_input.index(line)] = line[:column] + "X" + line[column + 1:]
 
     line_length = len(main_input[0])
 
     for row in sorted(list(empty_rows), reverse=True):
-        main_input.insert(row, line_length * "X")
+        main_input[row] = line_length * "X"
 
-    for row in main_input:
-        print(row)
 
 def find_all_galaxies(task_input):
 
@@ -37,7 +35,28 @@ def find_all_galaxies(task_input):
 
     return galaxy_positions
 
-def part_one(task_input):
+
+def count_empty_interceptions(coordinate1, coordinate2):
+
+    interception_count = 0
+    x1, y1 = coordinate1
+    x2, y2 = coordinate2
+
+    x_min, x_max = min(x1, x2), max(x1, x2)
+    y_min, y_max = min(y1, y2), max(y1, y2)
+
+    for column in empty_columns:
+        if x_min < column < x_max:
+            interception_count += 1
+
+    for row in empty_rows:
+        if y_min < row < y_max:
+            interception_count += 1
+
+    return interception_count
+
+
+def main(task_input, empty_space_size):
 
     sum = 0
     galaxy_coordinates = find_all_galaxies(task_input)
@@ -47,7 +66,10 @@ def part_one(task_input):
         for i in range(2):
             sum += abs(coordinate_tuple[0][i] - coordinate_tuple[1][i])
         
+        sum += count_empty_interceptions(*coordinate_tuple) * empty_space_size
+
     return sum
 
 
-print(part_one(main_input))
+print("Part 1:", main(main_input, 1))
+print("Part 2:", main(main_input, 1e6-1))
